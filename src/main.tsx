7debdecdb92b67e5c4ded6097cbd3ad27089e5f6
vikasparth <vikasparth@gmail.com>
2026-04-14 11:35:29 -0700

    feat: add Sentry error tracking to frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+import * as Sentry from "@sentry/react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.MODE,
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
